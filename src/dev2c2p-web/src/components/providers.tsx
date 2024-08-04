4317014ebbaef70aad9bc02e53577e87d5d6ae5f
Gunn Bhatrakarn <gunn.bha@gmail.com>
2024-08-05 00:02:22 +0700

    feat(setup): initialize web project for testing

---
+++ src/dev2c2p-web/src/components/providers.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { NextUIProvider } from '@nextui-org/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { PropsWithChildren } from 'react';
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: Readonly<PropsWithChildren>) {
+  return (
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </NextThemesProvider>
+    </NextUIProvider>
+  );
+}
